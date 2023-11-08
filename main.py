bb56fa4fbaa4087bd85eb83d270a476725b1db57
Aleksandra Karwat <62639078+funnyCantaloupe@users.noreply.github.com>
2023-11-08 22:05:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def needleman_wunsch(sequence1, sequence2, match=1, mismatch=-1, gap_penalty=-2)
     aligned_seq1, aligned_seq2 = "", ""
     i, j = rows - 1, cols - 1
 
+    # wyznaczenie sciezki
     while i > 0 or j > 0:
         if i > 0 and score_matrix[i][j] == score_matrix[i - 1][j] + gap_penalty:
             aligned_seq1 = sequence1[i - 1] + aligned_seq1
@@ -37,6 +38,8 @@ def needleman_wunsch(sequence1, sequence2, match=1, mismatch=-1, gap_penalty=-2)
     return aligned_seq1, aligned_seq2, score_matrix[rows - 1][cols - 1]
 
 
+
+
 def read_fasta_file(file_path):
     sequences = []
     with open(file_path, 'r') as file:
@@ -46,22 +49,28 @@ def read_fasta_file(file_path):
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Perform Needleman-Wunsch alignment on two amino acid sequences from a FASTA file.')
-    parser.add_argument('file_path', metavar='file_path', type=str,
-                        help='Path to the FASTA file containing the sequences')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file_path')
     args = parser.parse_args()
 
     sequences = read_fasta_file(args.file_path)
     if len(sequences) != 2:
-        print("Error: Input file should contain exactly two sequences.")
+        print("Error: Nieprawidlowa liczba sekwencji.")
         return
 
     sequence1, sequence2 = sequences
     aligned_seq1, aligned_seq2, alignment_score = needleman_wunsch(sequence1, sequence2)
 
-    print("Optymalne dopasowanie 1:", aligned_seq1)
-    print("Optymalne dopasowanie 2:", aligned_seq2)
+    # Otwarcie pliku do zapisu wyników
+    with open('wynik.txt', 'w') as result_file:
+        result_file.write("1: {}\n".format(aligned_seq1))
+        result_file.write("2: {}\n".format(aligned_seq2))
+        result_file.write("Alignment Score: {}\n".format(alignment_score))
+
+    print("Dopasowanie:")
+    print(aligned_seq1)
+    print("\n")
+    print(aligned_seq2)
     print("Alignment Score:", alignment_score)
 
 
